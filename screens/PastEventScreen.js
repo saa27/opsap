02d12e3b33baf6670986fe19561ca7c66150e202
saa27 <saakshi27050@gmail.com>
2020-10-04 15:34:13 +0530

    added past and ongoing events screen

--- screens/PastEventScreen.js
+++ screens/PastEventScreen.js
@@ -1,13 +1,55 @@
 import React from "react";
-import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
+const { width, height } = Dimensions.get("window");
 
 const PastEventScreen = (props) => {
   return (
-    <ScrollView>
-      <Image />
+    <ScrollView style={styles.screen}>
+      <View style={styles.container}>
+        <Text style={styles.subHeader2}>Past Events.</Text>
+      </View>
+      <ImageBackground
+        style={styles.imageBg}
+        source={require("../assets/images/events/past_events/campus_soop_7.jpg")}
+      ></ImageBackground>
+      <ImageBackground
+        style={styles.imageBg}
+        source={require("../assets/images/events/past_events/campus_soop_8.jpg")}
+      ></ImageBackground>
     </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  subHeader2: {
+    color: "#C61422",
+    paddingHorizontal: 10,
+    fontSize: 30,
+    paddingBottom: 17,
+    fontFamily: "roman",
+  },
+  container: {
+    borderLeftWidth: 5,
+    borderLeftColor: "#C61422",
+    marginLeft: 20,
+    marginVertical: 20,
+    paddingTop: 5,
+  },
+  imageBg: {
+    height: 550,
+    width: width,
+    // marginBottom: 15,
+  },
+});
 export default PastEventScreen;
